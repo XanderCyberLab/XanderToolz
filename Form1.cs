01e5f7bc9d249f01708000358f73fa4478f4b1df
Wishymashy <amoralesmm@yahoo.com>
2021-09-17 10:57:49 -0500

    Working on DLL String Format Function

--- Form1.cs
+++ Form1.cs
@@ -407,9 +407,8 @@ namespace XanderToolz
             double angleMinutes = Double.Parse(minutes.Text);
             double angleSeconds = Double.Parse(seconds.Text);
             Double degCalx = angle + (angleMinutes / 60) + (angleSeconds / 3600);
-            degAnswer.Text = XanderToolzDLL.MathEquations.RemoveLastFour(formatted: degCalx.ToString());
-
-            //degAnswer.Text = degCalx.ToString();
+            //degAnswer.Text = XanderToolzDLL.MathEquations.RemoveLastFour(formatted: degCalx.ToString());
+            degAnswer.Text = degCalx.ToString();
         }
 
     }
